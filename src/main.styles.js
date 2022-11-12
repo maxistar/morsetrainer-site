e80c8232a8f1060aaafd17ef30bef59c2b7911b1
Max Starikov <maxim.starikov@gmail.com>
2022-11-12 19:59:09 +0100

    add css

--- src/main.styles.js
+++ src/main.styles.js
@@ -1,3 +1,5 @@
-import '../styles/reset.css';
+//import '../styles/reset.css';
 import '../styles/morsekey.css';
+//import '../styles/texts.scss';
+import '../styles/layout.css';
 // import onDocumentLoad from './morsepage';
