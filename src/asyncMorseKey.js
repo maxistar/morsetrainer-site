a98aebc7446af38d10f08f3b5d9251fcc197f3c0
Max Starikov <maxim.starikov@gmail.com>
2023-01-10 23:11:05 +0100

    add async key

---
+++ src/asyncMorseKey.js
@@ -0,0 +1,108 @@
+const MODE_IDLE = 0;
+const MODE_DOT = 1;
+const MODE_DASH = 2;
+const MODE_DELETE = 3;
+const MODE_CHANGE_ALPHABET = 4;
+const PAUSE_LETTER = 5;
+const PAUSE_SPACE = 6;
+
+
+/**
+ * clicked
+ * MODE_DOT --> MODE_DASH --> MODE_DELETE --> MODE_CHANGE_APLHABET --> MODE_IDLE
+ * PAUSE_LETTER --> PAUSE_SPACE
+ */
+class AsyncMorseKey {
+
+    #debounceTimeout = 10;
+
+    #pointLength = 500;
+
+    #dashLength = 1500;
+
+    #deleteLength = 2500;
+
+    #switchAlphabetLength = 3500;
+
+    #characterSpacing = 800;
+
+    #pauseTime = 1500;
+
+    #reader;
+
+    #clickedTime = -1;
+
+    #releasedTime = -1;
+
+    #currentMode = MODE_IDLE;
+
+    constructor(reader) {
+        this.#reader = reader;
+    }
+
+    getDotLength() {
+        return this.#pointLength;
+    }
+
+    getDashLength() {
+        return this.#dashLength;
+    }
+
+    getDeleteLength() {
+        return this.#deleteLength;
+    }
+
+    getSwitchAlphabetLength() {
+        return this.#switchAlphabetLength;
+    }
+
+    getCharacterSpacingLength() {
+        return this.#characterSpacing;
+    }
+
+    getPauseLength() {
+        return this.#pauseTime;
+    }
+
+
+    pressKey() {
+        //if (this.#clickedTime !== -1) {
+        //    return;
+        //}
+        //this.#clickedTime = (new Date()).getTime();
+
+        //if (this.#releasedTime !== -1) {
+        //    if (this.#clickedTime - this.#releasedTime > this.#pauseTime) {
+        //        this.#reader.addPause();
+        //        this.#reader.addSpace();
+        //    } else if (this.#clickedTime - this.#releasedTime > this.#characterSpacing) {
+        //        this.#reader.addPause();
+        //    }
+        //}
+        this.#reader.addDot();
+    }
+
+    releaseKey() {
+        if (this.#clickedTime === -1) {
+            return;
+        }
+        const newTime = (new Date()).getTime();
+        
+        if (newTime - this.#clickedTime < this.#pointLength) {
+            this.#reader.addDot();
+        } else if (newTime - this.#clickedTime < this.#dashLength) {
+            this.#reader.addDash();
+        } else if (newTime - this.#clickedTime < this.#deleteLength) {
+            this.#reader.deleteLastCharacter();
+        } else {
+            this.#reader.toggleAlphabet();
+        }
+
+        this.#releasedTime = newTime;
+        
+        this.#clickedTime = -1;
+    }
+
+}
+
+export default AsyncMorseKey;
